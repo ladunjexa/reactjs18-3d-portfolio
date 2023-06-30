d8ffc833db11dc39d9c0178570c1c82011fa8452
awelrisak <105218428+awelrisak@users.noreply.github.com>
2023-06-30 12:49:44 +0300

    Delete src directory

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
--- src/hoc/index.js
+++
@@ -1,3 +0,0 @@
-import SectionWrapper from "./SectionWrapper";
-
-export { SectionWrapper };
