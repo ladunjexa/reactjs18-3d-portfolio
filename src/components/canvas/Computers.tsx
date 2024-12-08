ff7bba2b2258eb85042b803e5d235e0f59b23446
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2024-12-08 14:12:02 +0200

    feat(orbit-controls): disable panning by setting `enablePan` to `false`

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -67,6 +67,7 @@ const ComputersCanvas = () => {
         >
           <Suspense fallback={<CanvasLoader />}>
             <OrbitControls
+              enablePan={false}
               enableZoom={false}
               maxPolarAngle={Math.PI / 2}
               minPolarAngle={Math.PI / 2}
