e74de58960019a25da9fa95286c7f4495f0bd67d
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2024-12-08 14:12:12 +0200

    feat(orbit-controls): disable panning by setting `enablePan` to `false`

--- src/components/canvas/Earth.tsx
+++ src/components/canvas/Earth.tsx
@@ -29,6 +29,7 @@ const EarthCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
+          enablePan={false}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
